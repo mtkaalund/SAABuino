42df3fef115b40e9de9d299ce6c62687ef26aebc
Michael Torp Kaalund <kaalund@gmail.com>
2019-12-08 21:33:32 +0100

    begun code for follow me home for the saab

---
+++ SAABuino/SAABuino.ino
@@ -0,0 +1,48 @@
+
+/*
+ *  follow me home light
+  virker ved at aktiverer langt lys imens fører døren er åben og nøglen er taget ud af tændingen, opgave, indgang på SAABuino,
+  signal fra tændingslås nøgle ude, B+ ben 7 i DICE (TWICE ben 17) når fører døren åbnes kommer der + på ben 1 i DICE puls 
+  udgang til DICE ben 41 tænder nærlys når døren lukkes igen program beskrivelse: B+ DICE ben 7 forsvinder når nøglen tages 
+  ud & + fra DICE ben 1 ind på SAABuino i den rækkefølge skal give en puls ud til DICE ben 41
+ */
+#define DICE_PIN_1    // Driver door signal
+#define DICE_PIN_7    // Key is out
+#define DICE_PIN_41   // Close light 
+
+typedef enum {
+  FOLLOW_ME_HOME,
+  AUTOINTERVAL_REARWINDOW,
+  NONE
+} states;
+
+volatile states state;
+
+void setup() {
+  // put your setup code here, to run once:
+  state = NONE;
+  pinMode(DICE_PIN_1, INPUT);
+  pinMode(DICE_PIN_7, INPUT);
+}
+
+void loop() {
+  if( digitalRead(DICE_PIN_1) && !digitalRead(DICE_PIN_7) ) {
+    // Change state to follow me home light
+    state = FOLLOW_ME_HOME;
+  }
+
+  switch( state )
+  {
+    case FOLLOW_ME_HOME:  
+    // When the key is out and the driver door is open. When DICE pin 41 is pulsed, the DICE should keep the lights on for 20 - 50 seconds as normal.
+    // This can be reprogrammed with the TECH2 SAAB tool.
+      pinMode(DICE_PIN_41, OUTPUT);
+      digitalWrite(DICE_PIN_41, HIGH);
+      delay(10); // Wait 10 ms before going low
+      digitalWrite(DICE_PIN_41, LOW);
+      pinMode(DICE_PIN_41, INPUT);
+    break; 
+  }
+
+  delay(200); // This is code is run every 200 ms
+}
