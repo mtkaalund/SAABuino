be82c803ee66349557695fea744f7511a71f5db3
Michael Torp Kaalund <kaalund@gmail.com>
2019-12-08 22:59:51 +0100

    Code for autointerval rear wiper has been made, still waiting for test

--- SAABuino/SAABuino.ino
+++ SAABuino/SAABuino.ino
@@ -1,10 +1,18 @@
-
+#include <TimeInterrupt.h>
 /*
 This project is build on https://hackaday.io/project/158595/gallery#c95bac70f561ae865c572f3897c6257d
 The idea is to add some more functionalty to our beloved SAABs.
 
 As the original project have implmented blink 3 times, 
- */
+in this project there are going to be implemnted
+
+* Follow Me Home Light
+* Autointerval rear window
+
+
+Arduino Libraries that are used in this projects:
+https://github.com/matthew-dickson-epic/TimeInterrupt
+*/
 #define DICE_PIN_1    4 // Driver door signal
 #define DICE_PIN_7    5 // Key is out
 #define DICE_PIN_19   6 // Rear window wiper
@@ -19,6 +27,8 @@ typedef enum {
   NONE
 } states;
 
+bool has_autointerval_rearwindow = false;
+
 volatile states state, old_state;
 
 void setup() {
@@ -27,6 +37,7 @@ void setup() {
   old_state = NONE;
   pinMode(DICE_PIN_1, INPUT);
   pinMode(DICE_PIN_7, INPUT);
+  pinMode(DICE_PIN_27, INPUT);
 }
 
 void loop() {
@@ -34,6 +45,18 @@ void loop() {
     // Change state to follow me home light
     old_state = state;
     state = FOLLOW_ME_HOME;
+  }
+
+  if( digitalRead(DICE_PIN_27) ) {
+    // Front window wiper is on
+    old_state = state;
+    state = AUTOINTERVAL_REARWINDOW;
+  }
+
+  if( (! digitalRead(DICE_PIN_27) ) && ( has_autointerval_rearwindow == true) )
+  {
+    TimeInterrupt.end();
+    has_autointerval_rearwindow = false;
   }
 
   switch( state )
@@ -45,12 +68,19 @@ void loop() {
       digitalWrite(DICE_PIN_41, HIGH);
       delay(1); // Wait 1 ms before going low
       digitalWrite(DICE_PIN_41, LOW);
+      delay(1);
+      digitalWrite(DICE_PIN_41, HIGH);
       pinMode(DICE_PIN_41, INPUT);
       state = old_state;
       old_state = NONE;
     break; 
     
     case AUTOINTERVAL_REARWINDOW:
+      has_autointerval_rearwindow = true;
+      TimeInterrupt.begin(NORMAL);
+      TimeInterrupt.addInterrupt(autointerval_rearwiper, 1000*6*5);
+      state = old_state;
+      old_state = NONE;
     break;
 
     case BLINK_RIGHT:
@@ -61,4 +91,11 @@ void loop() {
   }
 
   delay(200); // This is code is run every 200 ms
+}
+
+void autointerval_rearwiper() {
+  pinMode(DICE_PIN_19, OUTPUT);
+  digitalWrite(DICE_PIN_19, LOW);
+  delay(2);
+  digitalWrite(DICE_PIN_19, HIGH);
 }
