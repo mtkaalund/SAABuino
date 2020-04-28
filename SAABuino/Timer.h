7d07ce7b8e38558fc8e75dc66b74a4f38c39d9b6
Michael Torp Kaalund <kaalund@gmail.com>
2020-04-28 22:14:56 +0200

    Timer.h, change add_100ms to update. TimerUpdate

--- SAABuino/Timer.h
+++ SAABuino/Timer.h
@@ -17,54 +17,72 @@
  * - Blink 3 times for lane change
 */
 
+// If Time between is not defined, then define it to be 100ms
+#ifndef TIMEBETWEEN
+#define TIMEBETWEEN 100
+#endif
 
 // Time holder
 struct Timer
 {
-  uint16_t milisecond;
-  uint8_t second;
-  uint8_t minute;
-  uint8_t hour;  
-  
-  Timer() {
-    milisecond = 0;
-    second = 0;
-    minute = 0;
-    hour = 0;  
-  }
-  
-  void add_100ms()
-  {
-    milisecond += 100;
-    if( milisecond >= 1000 )
+    uint16_t milisecond;
+    uint8_t second;
+    uint8_t minute;
+    uint8_t hour;
+
+    Timer()
     {
-      milisecond -= 1000;
-      second += 1;
-      if(second >= 60)
-      {
-        second -= 60;
-        minute += 1;
-        if( minute >= 60 )
-        {
-          minute -= 60;
-          hour += 1;
-        }
-      }
+        milisecond = 0;
+        second = 0;
+        minute = 0;
+        hour = 0;
     }
-  }
-  
-  void print_time()
-  {
-    Serial.print("ms: ");
-    Serial.println(milisecond);
-    Serial.print("s: ");
-    Serial.println(second);
-    Serial.print("m: ");
-    Serial.println(minute);
-    Serial.print("h: ");
-    Serial.println(hour);
-  }
+
+    void update()
+    {
+        milisecond += TIMEBETWEEN;
+        if (milisecond >= 1000)
+        {
+            milisecond -= 1000;
+            second += 1;
+            if (second >= 60)
+            {
+                second -= 60;
+                minute += 1;
+                if (minute >= 60)
+                {
+                    minute -= 60;
+                    hour += 1;
+                }
+            }
+        }
+    }
+
+    void print_time()
+    {
+        Serial.print("ms: ");
+        Serial.println(milisecond);
+        Serial.print("s: ");
+        Serial.println(second);
+        Serial.print("m: ");
+        Serial.println(minute);
+        Serial.print("h: ");
+        Serial.println(hour);
+    }
 };
 
 Timer internal_timer;
+
+// Our holding timer
+unsigned long count_time;
+
+void UpdateTime()
+{
+    if ((millis() - count_time) > TIMEBETWEEN)
+    {
+        internal_timer.update();
+        count_time = millis();
+    }
+}
+
 #endif
