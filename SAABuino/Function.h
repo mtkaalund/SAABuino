dbbebb317dfc530473a38017ef34b0fa46c6d408
Michael Torp Kaalund <kaalund@gmail.com>
2020-05-03 23:53:25 +0200

    Added a virtual class Function and a test class

---
+++ SAABuino/Function.h
@@ -0,0 +1,36 @@
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+#include "Timer.h"
+#include "DICE.h"
+
+class Function
+{
+public:
+    struct time_tm run_time;
+    Function()
+    {
+        run_time.ms = 0;
+        run_time.s = 0;
+        run_time.m = 0;
+        run_time.h = 0;
+    }
+
+    virtual void run();
+};
+
+class TestFunc : public Function
+{
+public:
+    TestFunc()
+    {
+        run_time.s = 10;
+    }
+
+    void run() override
+    {
+        Serial.println("This should be run every 10s");
+    }
+};
+
+#endif
